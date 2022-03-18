63ee5af1f78956775054de9b90d13f9fe6e17faa
Tay Weida <weida.tay99@gmail.com>
2022-03-18 17:50:27 +0800

    Revert changes

--- Objects/ledcontrols.d
+++ Objects/ledcontrols.d
@@ -1,4 +1,5 @@
 .\objects\ledcontrols.o: ledControls.c
+<<<<<<< HEAD
 .\objects\ledcontrols.o: C:\Users\diego\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
 .\objects\ledcontrols.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\ledcontrols.o: C:\Users\diego\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0plus.h
@@ -9,4 +10,16 @@
 .\objects\ledcontrols.o: ledControls.h
 .\objects\ledcontrols.o: C:\Users\diego\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
 .\objects\ledcontrols.o: C:\Users\diego\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\RTOS2\Include\cmsis_os2.h
+=======
+.\objects\ledcontrols.o: C:\Users\User\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
+.\objects\ledcontrols.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\ledcontrols.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\ledcontrols.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\ledcontrols.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\ledcontrols.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\ledcontrols.o: .\RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h
+.\objects\ledcontrols.o: ledControls.h
+.\objects\ledcontrols.o: C:\Users\User\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
+.\objects\ledcontrols.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\RTOS2\Include\cmsis_os2.h
+>>>>>>> parent of af13f36 (Delete Objects directory)
 .\objects\ledcontrols.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
--- Objects/uart.d
+++ Objects/uart.d
@@ -1,4 +1,5 @@
 .\objects\uart.o: uart.c
+<<<<<<< HEAD
 .\objects\uart.o: C:\Users\diego\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
 .\objects\uart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\uart.o: C:\Users\diego\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0plus.h
@@ -8,3 +9,14 @@
 .\objects\uart.o: .\RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h
 .\objects\uart.o: uart.h
 .\objects\uart.o: C:\Users\diego\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
+=======
+.\objects\uart.o: C:\Users\User\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
+.\objects\uart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\uart.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\uart.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\uart.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\uart.o: C:\Users\User\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\uart.o: .\RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h
+.\objects\uart.o: uart.h
+.\objects\uart.o: C:\Users\User\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
+>>>>>>> parent of af13f36 (Delete Objects directory)
