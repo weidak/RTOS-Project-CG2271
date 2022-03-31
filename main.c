1d793374f91cafd1732efa3e7d4cdb9de17159fe
e0550472@u.nus.edu <tryyang2001@gmail.com>
2022-04-01 00:47:52 +0800

    abstract ultasonic out from main

--- main.c
+++ main.c
@@ -196,91 +196,6 @@ void offRed() {
 	PTB->PDOR |= MASK(RED_LED);
 }
 
-/*******************
-	ULTRASONIC SENSOR
-********************/
-#define TRIGGER_PIN 30
-#define ECHO_PIN 31
-#define AIR_SPEED 34000
-
-void InitUltra() {
-	SIM_SCGC5 |= SIM_SCGC5_PORTE_MASK;
-	SIM_SCGC6 |= SIM_SCGC6_TPM0_MASK | SIM_SCGC6_PIT_MASK;
-	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;	
-	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1); 	
-	//Configure PIT for 10 us pulse generation
-	PIT->MCR &= ~PIT_MCR_MDIS_MASK;
-	PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK;
-	PIT->CHANNEL[0].LDVAL = 2000;//for 10us pulse
-	//Configure TPM0 to input capture mode
-	TPM0->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK) | TPM_SC_CPWMS_MASK);
-	TPM0->SC |= TPM_SC_CMOD(1) | TPM_SC_PS(7);
-	TPM0->MOD = 3750000; //10 s timer, will reset every 1 s
-	TPM0_C4SC |= TPM_CnSC_CHIE_MASK | TPM_CnSC_ELSA(1) | TPM_CnSC_ELSB(1);
-	//GPIO configuration
-	PORTE->PCR[TRIGGER_PIN] &= ~PORT_PCR_MUX_MASK;
-	PORTE->PCR[TRIGGER_PIN] |= PORT_PCR_MUX(1);
-	PORTE->PCR[ECHO_PIN] &= ~PORT_PCR_MUX_MASK;
-	PORTE->PCR[ECHO_PIN] |= PORT_PCR_MUX(3);
-	//Set GPIO direction
-	PTE->PDDR |= MASK(TRIGGER_PIN);
-	PTE->PDDR &= ~MASK(ECHO_PIN);
-}
-
-volatile int firstTime = 0;
-volatile int flagTRIGGER, flagRising, flagFalling = 0;
-
-void PIT_IRQHandler() { 
-	//Clear Pending IRQ
-	NVIC_ClearPendingIRQ(PIT_IRQn);
-	//Configure output pin
-	if (firstTime == 0) {
-		flagTRIGGER = 1;
-		PTD->PDOR |= MASK(TRIGGER_PIN);
-	}
-	else if (firstTime == 1) {
-		PTD->PDOR &= ~MASK(TRIGGER_PIN);
-		PIT->MCR &= ~PIT_TCTRL_TEN_MASK;
-		//NVIC_DisableIRQ(PIT_IRQn);
-	}
-	firstTime = ~firstTime;
-	//Clear Flag
-	PIT->CHANNEL[0].TFLG &= PIT_TFLG_TIF_MASK;
-}
-
-volatile int counter = 0;
-volatile uint32_t start_time = 0;
-volatile uint32_t stop_time = 0;
-
-void TPM0_IRQHandler() {
-	if (counter % 2 == 0) {
-		//rising edge
-		flagRising = 1;
-		start_time = TPM0_C4V;
-	}
-	else {
-		flagFalling = 1;
-		stop_time = TPM0_C4V;
-	}
-	counter = ~counter;
-	//Clear Flag
-	TPM0_C4SC |= TPM_CnSC_CHF_MASK;
-}
-
-float getDistance() {
-	PTE->PDOR |= MASK(TRIGGER_PIN);
-	delay(0x100);
-	PTE->PDOR &= ~MASK(TRIGGER_PIN);
-	delay(0x3333);//ard 30 ms delay to read start and stop time
-	uint32_t start = start_time;
-	uint32_t stop = stop_time;
-	float time = ((float)(stop_time - start_time)) / 375000.0;
-	float distance = AIR_SPEED * time / 2;
-	int i = 0;
-	return distance;
-
-}
-
 int main() {
 	SystemCoreClockUpdate();
 	initGPIOLed();
@@ -308,13 +223,10 @@ int main() {
 		//Reset counter
 		//start_time = 0;
 		//stop_time = 0;
-		int flagReset = flagTRIGGER;
 		TPM0_SC &= ~TPM_SC_CMOD_MASK;
 		TPM0->SC |= TPM_SC_CMOD(1);
 		TPM0_C4SC |= TPM_CnSC_CHIE_MASK | TPM_CnSC_ELSA(1) | TPM_CnSC_ELSB(1);
 		TPM0_CNT = 0;
-		counter = 0;
-		firstTime = 0;
 		PIT->CHANNEL[0].LDVAL = 104;
 		//Enable interrupts for PIT and TPM0
 		NVIC_EnableIRQ(TPM0_IRQn);
@@ -326,8 +238,6 @@ int main() {
 		else{
 			offRed();
 		}
-		int flag = flagTRIGGER;
-		flagTRIGGER = 0;
 		int i = 0;
 	}
 
